24a6b8a484f69dedc03c22da1709324bab67005b
davsaint89 <61285898+davsaint89@users.noreply.github.com>
2021-06-20 08:28:21 -0500

    'Version estable para los dos turnos'

--- reto_3.py
+++ reto_3.py
@@ -11,10 +11,13 @@ import datetime
 from datetime import date, datetime, time, timedelta
 
 contador = 1
+contador1 = 1
 compras = list()
+compras2 = list()
 datos_clientes = list()
 i = 0
 j = 0
+k = 0
 
 def datos_cliente():
     opt = 99
@@ -50,7 +53,7 @@ def datos_cliente():
 
     print(len(datos_clientes))
 
-def calculo(forma, tipo):
+def calculo_turno_1(forma, tipo):
     global contador
     global compras
     global i
@@ -62,7 +65,7 @@ def calculo(forma, tipo):
     total = 0
     while opb != 2:
 
-        hoy= datetime.today()
+        hoy= datetime.now()
         print(hoy.time())
         #print(hoy.time() > datetime.time(12,0,0))
         formatdate= "%d/%m/%Y %H:%M:%S"
@@ -71,6 +74,8 @@ def calculo(forma, tipo):
         compras[i].append(contador)
         compras[i].append(now.split()[0])
         compras[i].append(now.split()[1])
+        compras[i].append(forma)
+        compras[i].append(tipo)
         while(op2!=2):
 
             print("Ingrese la referencia del producto: ")
@@ -160,51 +165,212 @@ def calculo(forma, tipo):
         op2 = 33
 
 
-def main():
-    opcion = 99
-    while(opcion != 6):
-        try:
-            print("Menú de Opciones\n1.Ingreso Datos del cliente\n2.Ingreso Datos para cálculo de la compra e impresión resumen de la compra\n3. Impresion Cantidad de compras por turno\n4.Impresion valor compras por turno\n5.Datos del envío\n6.Salir")
-            opcion=int(input("> "))
+def calculo_turno_2(forma, tipo):
+    global contador1
+    global compras2
+    global k
+    op2 = 88
+    opb = 89
+    subtotal = 0
+    descuento_cant = 0
+    valor_compra = 0
+    total = 0
+    while opb != 2:
 
-            if opcion == 1:
-                datos_cliente()
+        hoy= datetime.now()
+        print(hoy.time())
+        #print(hoy.time() > datetime.time(12,0,0))
+        formatdate= "%d/%m/%Y %H:%M:%S"
+        now = hoy.strftime(formatdate)
+        compras2.append([])
+        compras2[k].append(contador1)
+        compras2[k].append(now.split()[0])
+        compras2[k].append(now.split()[1])
+        compras2[k].append(forma)
+        compras2[k].append(tipo)
+        while(op2!=2):
 
-            elif opcion == 2:
+            print("Ingrese la referencia del producto: ")
+            referencia_p = str(input("> "))
+            compras2[k].append(referencia_p)
+            print("Ingrese nombre del producto: ")
+            nombre_p = str(input("> "))
+            compras2[k].append(nombre_p)
+            print("Ingrese cantidad de producto: ")
+            cantidad_p = int(input("> "))
+            compras2[k].append(cantidad_p)
+            print("Ingrese precio del producto: ")
+            precio_p = int(input("> "))
+            compras2[k].append(precio_p)
 
-                print("Ingrese la forma me envío: 1= Rápido   2=Normal")
-                forma_envio=int(input("> "))
-
-                if forma_envio >= 1: # Rápido
-                    print("Ingrese la forma me envío: 1=Local, 2=Nacional, 3=Internacional")
-                    tipo_envio=int(input("> "))
-                    #print(tipo_envio)
-                    if tipo_envio >= 1 and tipo_envio <=3:
-                        calculo(forma_envio, tipo_envio)
-
-                elif forma_envio == 2: # normal
-                    print("Ingrese la forma me envío: 1=Local, 2=Nacional, 3=Internacional")
-                    tipo_envio=int(input("> "))
-                    #print(tipo_envio)
-                    if tipo_envio >= 1 and tipo_envio <=3:
-                        calculo(forma_envio, tipo_envio)
-
-            elif opcion == 3:
-                print("Cantidad compras por turno")
-
-            elif opcion == 4:
-                print("Imprimir resumen compras por turno")
-
-            elif opcion == 5:
-                print("Pedir datos de envío")
+            if cantidad_p > 3:
+                descuento_cant += cantidad_p * precio_p * 0.05
+                subtotal += cantidad_p * precio_p
+                valor_compra = subtotal - descuento_cant
 
             else:
+                descuento_cant += 0
+                subtotal += cantidad_p * precio_p
+                valor_compra = subtotal
+
+            if forma == 1:
+                if tipo == 1:
+                    if valor_compra > 800000:
+                        envio = 0
+                        dscto_envio = 8000
+                    else:
+                        envio = 8000
+                        dscto_envio = 0
+
+                if tipo == 2:
+                    envio = 10000
+                    dscto_envio = 0
+
+                if tipo == 3:
+                    envio = 40000
+                    dscto_envio = 0
+
+            elif forma == 2:
+                if tipo == 1:
+                    if valor_compra > 500000:
+                        envio = 0
+                        dscto_envio = 4000
+                    else:
+                        envio = 4000
+                        dscto_envio = 0
+
+                if tipo == 2:
+                    if valor_compra >= 1000000:
+                        envio = 0
+                        dscto_envio = 5000
+                    else:
+                        envio = 5000
+                        dscto_envio = 0
+
+                if tipo == 3:
+                    if valor_compra >= 2000000:
+                        envio = 0
+                        dscto_envio = 20000
+                    else:
+                        envio = 20000
+                        dscto_envio = 0
+            print("Otro producto? (1=Si ; 2=No)")
+            compras2[k].append(int(subtotal))
+            compras2[k].append(int(descuento_cant))
+            compras2[k].append(int(envio))
+            total = subtotal + envio - descuento_cant
+            compras2[k].append(int(total))
+            op2 = int(input("> "))
+        print(compras2)
+        print(int(subtotal)) #Valor compra
+        print(int(descuento_cant)) # Valor descuento
+        print(int(envio))# Valor envio
+        print(int(total)) # Valor total a pagar
+        print("Otra compra:")
+        opb = int(input("> "))
+        contador1 += 1
+        k += 1
+        subtotal = 0
+        descuento_cant = 0
+        valor_compra = 0
+        total = 0
+        op2 = 33
+
+
+def main():
+    hoy = datetime.now()
+    hora_actual = hoy.time()
+    medio_dia = time(12,0,0,0)
+    opcion = 99
+    while(opcion != 6):
+        print(hora_actual < medio_dia)
+        if hora_actual > medio_dia:
+            print(" TURNO 1 ".center(60,'-'))
+            try:
+                print("Menú de Opciones\n1.Ingreso Datos del cliente\n2.Ingreso Datos para cálculo de la compra e impresión resumen de la compra\n3.Impresion Cantidad de compras por turno\n4.Impresion valor compras por turno\n5.Datos del envío\n6.Salir")
+                opcion=int(input("> "))
+
+                if opcion == 1:
+                    datos_cliente()
+
+                elif opcion == 2:
+
+                    print("Ingrese la forma me envío: 1= Rápido   2=Normal")
+                    forma_envio=int(input("> "))
+
+                    if forma_envio >= 1: # Rápido
+                        print("Ingrese la forma me envío: 1=Local, 2=Nacional, 3=Internacional")
+                        tipo_envio=int(input("> "))
+                        #print(tipo_envio)
+                        if tipo_envio >= 1 and tipo_envio <=3:
+                            calculo_turno_1(forma_envio, tipo_envio)
+
+                    elif forma_envio == 2: # normal
+                        print("Ingrese la forma me envío: 1=Local, 2=Nacional, 3=Internacional")
+                        tipo_envio=int(input("> "))
+                        #print(tipo_envio)
+                        if tipo_envio >= 1 and tipo_envio <=3:
+                            calculo_turno_1(forma_envio, tipo_envio)
+
+                elif opcion == 3:
+                    print("Cantidad compras por turno")
+
+                elif opcion == 4:
+                    print("Imprimir resumen compras por turno")
+
+                elif opcion == 5:
+                    print("Pedir datos de envío")
+
+                else:
+                    continue
+                        #print("la opcion ingresada no existe")
+            except:
                 continue
-                    #print("la opcion ingresada no existe")
-        except:
-            continue
+        else:
+            print(" TURNO 2 ".center(60,'-'))
+            try:
+                print("Menú de Opciones\n1.Ingreso Datos del cliente\n2.Ingreso Datos para cálculo de la compra e impresión resumen de la compra\n3.Impresion Cantidad de compras por turno\n4.Impresion valor compras por turno\n5.Datos del envío\n6.Salir")
+                opcion=int(input("> "))
+
+                if opcion == 1:
+                    datos_cliente()
+
+                elif opcion == 2:
+
+                    print("Ingrese la forma me envío: 1= Rápido   2=Normal")
+                    forma_envio=int(input("> "))
+
+                    if forma_envio >= 1: # Rápido
+                        print("Ingrese la forma me envío: 1=Local, 2=Nacional, 3=Internacional")
+                        tipo_envio=int(input("> "))
+                        #print(tipo_envio)
+                        if tipo_envio >= 1 and tipo_envio <=3:
+                            calculo_turno_2(forma_envio, tipo_envio)
+
+                    elif forma_envio == 2: # normal
+                        print("Ingrese la forma me envío: 1=Local, 2=Nacional, 3=Internacional")
+                        tipo_envio=int(input("> "))
+                        #print(tipo_envio)
+                        if tipo_envio >= 1 and tipo_envio <=3:
+                            calculo_turno_2(forma_envio, tipo_envio)
+
+                elif opcion == 3:
+                    print("Cantidad compras por turno")
+
+                elif opcion == 4:
+                    print("Imprimir resumen compras por turno")
+
+                elif opcion == 5:
+                    print("Pedir datos de envío")
+
+                else:
+                    continue
+                        #print("la opcion ingresada no existe")
+            except:
+                continue
 
 
 
 if __name__ == '__main__':
     main()
+    #calculo_turno_2(1, 2)
